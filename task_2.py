ec4027070fed3b60c0c844f9815b59f4f924aa01
andriiborodin <miserdam@gmail.com>
2025-10-01 21:52:28 +0300

    finished task 2

---
+++ task_2.py
@@ -0,0 +1,20 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000:
+        return []
+    if min > max:
+        return []
+    if quantity > (max - min + 1):
+        return []
+
+    random_set = set()
+    while len(random_set) < quantity:
+        random_set.add(random.randint(min, max))
+
+    return sorted(random_set)
+
+
+print("Ваші лотерейні числа:", get_numbers_ticket(1, 1000, 6))
+print("Ваші лотерейні числа:", get_numbers_ticket(1000, 1, 6))
+print("Ваші лотерейні числа:", get_numbers_ticket(99, 100, 3))
